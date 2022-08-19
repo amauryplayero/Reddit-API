4a6cfd4bc5d31e5c4372a2eedd545154864f7a81
Amaury <amaury.lopezdiaz@gmail.com>
2022-08-19 15:57:00 -0400

    it scrollsgit add .! A little wanky, but it does it without a loop. Made everything dependent on the custom hook. It's the one calling the use effect, setting the posts state, and handling the uncontrollable API calls.

--- src/Components/InfiniteLoop.js
+++ src/Components/InfiniteLoop.js
@@ -1,16 +1,31 @@
-// import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-// export default function InfiniteLoop(lastNameQuery) {
-
-//     useEffect(()=>{
-//         axios.get(`https://www.reddit.com/r/aww/.json?after=${lastNameQuery}`).then(
-//         (res) => {
-//             console.log(res.data)
-//             //  setPosts(prevState=>([...posts, ...res.data.data.children]))
-
-//     },[lastNameQuery])
+export default function HandleInfiniteLoop(lastNameQuery) {
+  const [loading, setLoading] = useState(false)
+  const[error, setError] = useState(false)
+  const [additionalPosts, setAdditionalPosts] = useState([])
 
 
 
-//   return null
-// }
+
+  let cancel
+  useEffect(()=>{
+    setLoading(true)
+    setError(false)
+        axios({
+        method: 'GET',
+        url:`https://www.reddit.com/r/aww/.json?after=${lastNameQuery}`,
+        cancelToken: new axios.CancelToken(c=> cancel = c)
+        }).then(res => {
+          setAdditionalPosts(prevAdditionalPosts=> ([...prevAdditionalPosts, ...res.data.data.children]))
+          })
+            //  setPosts(prevState=>([...posts, ...res.data.data.children]))   
+    
+    return () => cancel()
+  
+    },[lastNameQuery])
+    // console.log(additionalPosts)
+
+  return {loading, error, additionalPosts}
+}
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,94 +2,71 @@ import React, {useState, useEffect, useRef} from 'react'
 import axios from 'axios'
 import MapThroughPosts from './MapThroughPosts'
 import { createConstructor } from 'typescript'
-import InfiniteLoop from './InfiniteLoop'
+import HandleInfiniteLoop from './InfiniteLoop'
 
 export default function Landing() {
 
-    const [posts, setPosts] = useState(undefined)
     const arrayWithNewPosts = useRef([])
     const countersForEffect = useRef(0)
-    const loading = useRef(false)
-  
+    const [lastNameQuery, setLastNameQuery] = useState("")
+    const page = useRef(0)
+
+    const {
+        additionalPosts,
+        loading,
+        error
+    
+    } = HandleInfiniteLoop(lastNameQuery)
 
     let loadingLogo
- 
 
-    const performInitialCall = () =>{
-        axios.get("https://www.reddit.com/r/aww/.json").then(
-            (res)=>{setPosts(res.data.data.children)}
-        )
-    }
-    function isScrolling(e){
-        const scrollHeight = e.target.scrollingElement.scrollHeight
-        const scrollTop = e.target.scrollingElement.scrollTop
-        const clientHeight = e.target.scrollingElement.clientHeight
-        const innerHeight = e.target.scrollingElement.innerHeight
-        const offsetHeight = e.target.scrollingElement.offsetHeight
+    HandleInfiniteLoop(lastNameQuery)
 
-
-
-        if(innerHeight + scrollTop!==offsetHeight){
-          return;
-        }
-        else {
-          console.log("scrolling down");
-        }
-    }
-    // Make initial API calls
     useEffect(()=>{
-        if(countersForEffect.current<1){
-            performInitialCall()
-            countersForEffect.current++
-        }
-        
-    },[])
 
+    },[])
     
-    console.log(posts)
+
+
 
     const handleScroll = (e) =>{
-       
+        
         const scrollHeight = e.target.scrollingElement.scrollHeight
         const scrollTop = e.target.scrollingElement.scrollTop
         const clientHeight = e.target.scrollingElement.clientHeight
-        
+        // const observer = useRef
         // When user touches bottom
-        loading.current=true
         
-        if(clientHeight + scrollTop >= scrollHeight && posts!=undefined){
-            console.log('bottom')
-            let lastNameQuery = posts[posts.length-1].data.name
-
-            if(loading.current===true){
-           
-                axios.get(`https://www.reddit.com/r/aww/.json?after=${lastNameQuery}`).then(
-                    (res) => {
-                        
-                        setPosts(prevState=>([...prevState, ...res.data.data.children]))
-                        loading.current= false
-                        
-                    }
-                    )
-         
-                }
-
-        }  
-    }
-                
-  
-  
-
-    let link = 'https://www.reddit.com/'
-window.addEventListener('scroll',(e)=>{handleScroll(e)})
-
-   
-
-  return (
-    <>
+        
+        if(clientHeight + scrollTop >= scrollHeight && additionalPosts!=undefined){
+            // console.log('bottom')
+            if(page<1){
+            setLastNameQuery(additionalPosts[additionalPosts.length-1].data.name)
+            }else{
+                setLastNameQuery(additionalPosts[additionalPosts.length-1].data.name)
+                // setPosts(additionalPosts)
+                loadingLogo = <>
+                </>
+            }
+                }  
+            }
+            
+        if(loading){
+            loadingLogo=<span>loading</span>
+        }
+            console.log(additionalPosts)
+            
+            // HandleInfiniteLoop(lastNameQuery)
+            let link = 'https://www.reddit.com/'
+            window.addEventListener('scroll',(e)=>{handleScroll(e)})
+            
+            
+            
+            return (
+                <>
     {/* <button onClick={()=>{performInitialCall()}}></button> */}
     <div id="mainDiv" onScroll={e=>handleScroll(e)}>
-        <MapThroughPosts  posts={posts}/>
+        <MapThroughPosts  posts={additionalPosts}/>
     </div>
     {loadingLogo}
     </>
