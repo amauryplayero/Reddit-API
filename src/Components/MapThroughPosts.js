d64518a272aab9c736468766459dff1bfba8ea89
Amaury <amaury.lopezdiaz@gmail.com>
2022-08-22 15:34:45 -0400

    deleted unnecesary swtich statements

--- src/Components/MapThroughPosts.js
+++ src/Components/MapThroughPosts.js
@@ -5,11 +5,8 @@ import InfiniteLoop from './InfiniteLoop'
 
 export default function MapThroughPosts({posts}) {
 
-   
-
-
+    if(posts===undefined) return
     // Map through the response of the GET requests. 
-    if(posts!=undefined){
         let postsArray = posts
         let mappedPosts = postsArray.map((e)=>{
             let singlePostData = e.data
@@ -20,73 +17,18 @@ export default function MapThroughPosts({posts}) {
                             </div>
 
                             <span>{singlePostData.author}</span>
-                                <a className="linkText"href={`https://www.reddit.com`+singlePostData.permalink} target="_blank">{singlePostData.title}</a>
+                            <a className="linkText"href={`https://www.reddit.com`+singlePostData.permalink} target="_blank">{singlePostData.title}</a>
                             <span className="subredditNameText">{singlePostData.subreddit_name_prefixed}</span>
                     </div>
                     </>
 
-            switch(singlePostData.post_hint) {
-                // if post contains an image
-                case "image":
-                    return(
-                        <>
-                        <div className="postContainer">
+            return (<>
+                    <div className="postContainer">
                             <div className="titleThumbnailNameContainer">
                                 {info}
                             </div>
-
-                        </div>
-                        </>
-                       )
-                break;
-                case "video":
-                return(
-                    <>
-                    <div className="postContainer">
-                        <div className="titleThumbnailNameContainer">
-                                {info}
-                        </div> 
-                        <u>video</u>
-                    </div>
-                    </>
-                )
-                break;
-                case "link":
-                return(
-                    <>
-                    <div className="postContainer">
-                      
-                        <div className="titleThumbnailNameContainer">
-                                {info}
-                        </div>
-                    </div>
-
-                    </>
-                )
-                break;
-                case "hosted:video":
-                 return(
-                    <>
-                    <div className="postContainer">
-                        <div className="titleThumbnailNameContainer">
-                                {info}
-                        </div>
-                    </div>
-
-                    </>
-                )
-                break;
-                default:
-                    return(
-                        <>
-                        <div className="postContainer">
-                            {info}
-                        </div>
-    
-                        </>
-                    )
-                break;
-            }
+                     </div>
+            </>)
         })
         return (
             <>
@@ -97,4 +39,4 @@ export default function MapThroughPosts({posts}) {
         )
     }
 
-}
+
